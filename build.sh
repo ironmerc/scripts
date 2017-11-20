023d58d7644cd82a49ae29bb82ebb2ab435e4ddd
Amogh Lele <31761843+ATechnoHazard@users.noreply.github.com>
2017-11-20 18:25:55 +0530

    Add a build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# Starts the build process with specified variables
+# For example,
+#	build.sh name1
+#
+#
+
+
+echo "export USE_CCACHE=1" » ~/.bashrc
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+wait
+
+. build/env*
+wait
+
+lunch dot_woods-userdebug
+wait
+export KBUILD_BUILD_USER=Buildserver
+export KBUILD_BUILD_HOST=$1
+mka poly -j64
