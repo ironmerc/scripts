f68c9f50e25c31e75135994c213aa997d68c77b1
Akshat Sinha <sinhaakshat2012@gmail.com>
2017-11-21 20:08:00 +0530

    Create poly.sh

---
+++ poly.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Poly-OS/android_device_xiaomi_land-b nougat device/xiaomi/land
+git clone https://github.com/RiteshSaxena/android_kernel_xiaomi_msm8937.git -b cm-14.1 kernel/xiaomi/msm8937
+git clone https://github.com/RiteshSaxena/proprietary_vendor_xiaomi -b cm-14.1-land vendor/xiaomi
