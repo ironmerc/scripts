f32df8fabfca4d52cd6109c930962f1f32a9185e
Akshat Sinha <sinhaakshat2012@gmail.com>
2017-12-03 01:49:46 +0530

    Create los.sh

---
+++ los.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Poly-OS/android_device_xiaomi_land -b nougat device/xiaomi/land
+git clone https://github.com/RiteshSaxena/android_kernel_xiaomi_msm8937.git -b cm-14.1 kernel/xiaomi/msm8937
+git clone https://github.com/RiteshSaxena/proprietary_vendor_xiaomi -b cm-14.1-land vendor/xiaomi
+mv device/xiaomi/land/poly.mk device/xiaomi/land/lineage.mk
+mv device/xiaomi/land/poly.dependencies device/xiaomi/land/lineage.dependencies
+nano lineage.mk
