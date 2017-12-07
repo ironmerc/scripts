cf8894e3a0cc82f89409818a5217623493f6413a
Akshat Sinha <sinhaakshat2012@gmail.com>
2017-12-08 00:12:54 +0530

    Create hals.sh

---
+++ hals.sh
@@ -0,0 +1,4 @@
+
+git clone https://github.com/HridayHS/android_hardware_qcom_display -b lineage-15.0-caf-8996 hardware/qcom/display-caf/msm8937
+git clone https://github.com/HridayHS/android_hardware_qcom_media -b lineage-15.0-caf-8996 hardware/qcom/media-caf/msm8937
+git clone https://github.com/HridayHS/android_hardware_qcom_audio -b lineage-15.0-caf-8996 hardware/qcom/audio-caf/msm8937
