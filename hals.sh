3648348c5412c0d50eca058201418b93ecd6253f
Akshat Sinha <sinhaakshat2012@gmail.com>
2017-12-25 18:23:52 +0530

    Update hals.sh

--- hals.sh
+++ hals.sh
@@ -1,4 +1,6 @@
-
+rm -rf hardware/qcom/display-caf
+rm -rf hardware/qcom/media-caf
+rm -rf hardware/qcom/audio-caf
 git clone https://github.com/HridayHS/android_hardware_qcom_display -b lineage-15.0-caf-8996 hardware/qcom/display-caf/msm8937
 git clone https://github.com/HridayHS/android_hardware_qcom_media -b lineage-15.0-caf-8996 hardware/qcom/media-caf/msm8937
 git clone https://github.com/HridayHS/android_hardware_qcom_audio -b lineage-15.0-caf-8996 hardware/qcom/audio-caf/msm8937
