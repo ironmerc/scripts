16dd941de6c10e7d634746a7bafb9a67d8398600
Akshat Sinha <sinhaakshat2012@gmail.com>
2017-12-31 15:30:41 +0530

    Derp

--- cos.sh
+++ cos.sh
@@ -3,7 +3,7 @@ git clone https://github.com/Cosmic-OS/platform_frameworks_opt_telephony -b oreo
 rm -rf packages/services/Telecomm
 git clone https://github.com/Cosmic-OS/platform_packages_services_Telecomm -b oreo-mr1-ims packages/services/Telecomm
 rm -rf packages/services/Telephony
-git clone https://github.com/Cosmic-OS/platform_packages_services_Telephony-b oreo-mr1-ims packages/services/Telephony
+git clone https://github.com/Cosmic-OS/platform_packages_services_Telephony -b oreo-mr1-ims packages/services/Telephony
 rm -rf frameworks/opt/net/ims
 git clone https://github.com/Cosmic-OS/platform_frameworks_opt_net_ims -b oreo-mr1-ims frameworks/opt/net/ims
 rm -rf packages/apps/PhoneCommon
