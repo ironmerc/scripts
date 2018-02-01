22fcb3c9a95d97df6d085b1f3ca88f850d3b3c45
Akshat Sinha <sinhaakshat2012@gmail.com>
2018-02-01 17:22:02 +0530

    Create hals.sh
    Cuz fuck yeah

---
+++ hals.sh
@@ -0,0 +1,4 @@
+rm -rf h*/q*/media-caf h*/q*/audio-caf h*/q*/display-caf 
+git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937 hardware/qcom/display-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8937
