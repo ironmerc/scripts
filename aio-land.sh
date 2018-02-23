1e8b299ee593229265bb6f62fc7a9cb3c540b73b
Akshat Sinha <sinhaakshat2012@gmail.com>
2018-02-24 00:46:07 +0530

    Update kernel branch

--- aio-land.sh
+++ aio-land.sh
@@ -1,6 +1,6 @@
 rm -rf h*/q*/media-caf h*/q*/audio-caf h*/q*/display-caf device/xiaomi/land kernel/xiaomi/msm8937 vendor/xiaomi
 git clone https://github.com/TeamReloaded/android_device_xiaomi_land.git -b lineage-15.1 device/xiaomi/land
-git clone https://github.com/TeamReloaded/android_kernel_xiaomi_msm8937 -b oreo-upstream kernel/xiaomi/msm8937
+git clone https://github.com/TeamReloaded/android_kernel_xiaomi_msm8937 -b oreo kernel/xiaomi/msm8937
 git clone https://github.com/akshatsinha2002/proprietary_vendor_xiaomi -b lineage-15.1 vendor/xiaomi
 git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8937
 git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937 hardware/qcom/display-caf/msm8937
