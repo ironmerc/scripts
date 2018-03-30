e2eb20c07aef846ed05b09e82dbb241c1f295dbf
Akshat Sinha <sinhaakshat2012@gmail.com>
2018-03-30 22:52:15 +0530

    Update hals branch

--- aio-land.sh
+++ aio-land.sh
@@ -2,7 +2,7 @@ rm -rf h*/q*/media-caf h*/q*/audio-caf h*/q*/display-caf device/xiaomi/land kern
 git clone https://github.com/TeamReloaded/android_device_xiaomi_land.git -b lineage-15.1 device/xiaomi/land
 git clone https://github.com/TeamReloaded/android_kernel_xiaomi_msm8937 -b oreo kernel/xiaomi/msm8937
 git clone https://github.com/akshatsinha2002/proprietary_vendor_xiaomi -b lineage-15.1 vendor/xiaomi
-git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b oreo-mr1-8937 hardware/qcom/audio-caf/msm8937
-git clone https://github.com/TeamReloaded/hardware_qcom_display -b oreo-mr1-8937 hardware/qcom/display-caf/msm8937
-git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b oreo-mr1-8937 hardware/qcom/media-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_audio.git -b lineage-15.1-caf-8996 hardware/qcom/audio-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_display -b lineage-15.1-caf-8996 hardware/qcom/display-caf/msm8937
+git clone https://github.com/TeamReloaded/hardware_qcom_media.git -b lineage-15.1-caf-8996 hardware/qcom/media-caf/msm8937
 git clone https://github.com/TeamReloaded/packages_apps_Snap -b lineage-15.1 packages/apps/Snap
