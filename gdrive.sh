5e23bf86400472a799968441b1933a2a02aa0d76
Akshat Sinha <sinhaakshat2012@gmail.com>
2018-06-30 22:42:02 +0530

    Fix derp

--- gdrive.sh
+++ gdrive.sh
@@ -1,6 +1,6 @@
 cd ~
 wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
-mv uc\?id\* gdrive
+mv uc?id\* gdrive
 chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
 gdrive list
