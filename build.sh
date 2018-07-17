7e2688c3960ae16c8393e32f1037b9aaa442f02e
Akshat Sinha <sinhaakshat2012@gmail.com>
2018-07-17 15:46:52 +0530

    Misc changes

--- build.sh
+++ build.sh
@@ -1,21 +1,8 @@
-#! /bin/bash
-#
-# Starts the build process with specified variables
-# For example,
-#	build.sh name1
-#
-#
-
-
 echo "export USE_CCACHE=1" » ~/.bashrc
-prebuilts/misc/linux-x86/ccache/ccache -M 50G
-wait
-
+ccache -M 50G
 . build/env*
-wait
-
 lunch fh_X00TD-userdebug
 wait
-export KBUILD_BUILD_USER=akshat
+export KBUILD_BUILD_USER=Akshat
 export KBUILD_BUILD_HOST=TheRagingBeast
 make bacon -j24
