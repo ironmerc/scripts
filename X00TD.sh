e5e99bf2d4c55f5a033250ca68a234812a5b411b
Akshat Sinha <sinhaakshat2012@gmail.com>
2018-11-24 15:21:06 +0530

    FIX DA HALS REPO NAME

--- X00TD.sh
+++ X00TD.sh
@@ -2,6 +2,6 @@ git clone https://github.com/ASUS-X00TD/android_device_asus_X00TD -b lineage-16.
 git clone https://github.com/ASUS-X00TD/android_kernel_asus_sdm660 -b lineage-16.0 kernel/asus/sdm660
 git clone https://github.com/ASUS-X00TD/proprietary_vendor_asus -b lineage-16.0-test vendor/asus
 rm -rf h*/q*/media-caf h*/q*/display-caf h*/q*/audio-caf
-git clone https://github.com/LineageOS/hardware_qcom_media -b lineage-16.0-caf-8998 hardware/qcom/media-caf/msm8998
-git clone https://github.com/LineageOS/hardware_qcom_audio -b lineage-16.0-caf-8998 hardware/qcom/audio-caf/msm8998
-git clone https://github.com/LineageOS/hardware_qcom_display -b lineage-16.0-caf-8998 hardware/qcom/display-caf/msm8998
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-16.0-caf-8998 hardware/qcom/media-caf/msm8998
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-16.0-caf-8998 hardware/qcom/audio-caf/msm8998
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-16.0-caf-8998 hardware/qcom/display-caf/msm8998
