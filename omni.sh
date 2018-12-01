c059a663341043ae1db2ae2693911d991971682c
Akshat Sinha <sinhaakshat2012@gmail.com>
2018-12-01 18:23:01 +0530

    Initial script for omni

---
+++ omni.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/omnirom/android_hardware_qcom_audio-caf-msm8998 -b android-9.0 hardware/qcom/audio-caf-msm8998
+git clone https://github.com/omnirom/android_hardware_qcom_media-caf-msm8998 -b android-9.0 hardware/qcom/media-caf-msm8998
+git clone https://github.com/omnirom/android_hardware_qcom_audio-caf-msm8998 -b android-9.0 hardware/qcom/display-caf-msm8998
+git clone https://github.com/omnirom/android_external_ant-wireless_ant_native -b android-8.1 external/ant-wireless/ant_native
+git clone https://github.com/omnirom/android_external_ant-wireless_antradio-library -b android-8.1 external/ant-wireless/antradio-library
+git clone https://github.com/omnirom/android_external_ant-wireless_ant_service -b android-8.1 external/ant-wireless/ant_service
+git clone https://github.com/omnirom/android_external_json-c -b android-9.0 external/json-c
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_interfaces -b android-9.0 vendor/qcom/opensource/interfaces
+git clone https://github.com/omnirom/vendor_qcom_opensource_dataservices -b android-9.0 vendor/qcom/opensource/dataservices
