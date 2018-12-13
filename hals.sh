f888d186207ab3a43046eb3adb421974ef3d8647
Akshat Sinha <sinhaakshat2012@gmail.com>
2018-12-13 23:07:01 +0530

    script: Update hals branch

--- hals.sh
+++ hals.sh
@@ -1,4 +1,4 @@
 rm -rf h*/q*/media-caf h*/q*/audio-caf h*/q*/display-caf 
-git clone https://github.com/RiteshSaxena/hardware_qcom_audio -b lineage-15.1-caf-8996 hardware/qcom/audio-caf/msm8996
-git clone https://github.com/RiteshSaxena/hardware_qcom_display -b lineage-15.1-caf-8996 hardware/qcom/display-caf/msm8996
-git clone https://github.com/RiteshSaxena/hardware_qcom_media -b lineage-15.1-caf-8996 hardware/qcom/media-caf/msm8996
+git clone https://github.com/RiteshSaxena/hardware_qcom_audio -b pie-caf-8996 hardware/qcom/audio-caf/msm8996
+git clone https://github.com/RiteshSaxena/hardware_qcom_display -b pie-caf-8996 hardware/qcom/display-caf/msm8996
+git clone https://github.com/RiteshSaxena/hardware_qcom_media -b pie-caf-8996 hardware/qcom/media-caf/msm8996
