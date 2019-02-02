79fd9d4c7c15a7010678ec03dabc80e98a86ae78
Akshat Sinha <sinhaakshat2012@gmail.com>
2019-02-02 19:04:07 +0530

    Source Cloner : Update will latest source

--- X00T.sh
+++ X00T.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/ASUS-X00TD/android_device_asus_X00TD -b lineage-16.0-test device/asus/X00TD
-git clone https://github.com/ASUS-X00TD/android_kernel_asus_sdm660 -b lineage-16.0 kernel/asus/sdm660
-git clone https://github.com/ASUS-X00TD/proprietary_vendor_asus -b lineage-16.0-test vendor/asus
+git clone https://github.com/ironmerc/device_asus_X00T -b aosip device/asus/X00T
+git clone https://github.com/KudProject/kernel_asus_sdm660 -b lineage-16.0 kernel/asus/sdm660
+git clone https://github.com/ironmerc/vendor_asus vendor/asus
 rm -rf h*/q*/media-caf h*/q*/display-caf h*/q*/audio-caf
 git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-16.0-caf-8998 hardware/qcom/media-caf/msm8998
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-16.0-caf-8998 hardware/qcom/audio-caf/msm8998
