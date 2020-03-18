4d502d068794b192c3bc753e998162b36a26b719
Akshat Sinha <sinhaakshat2012@gmail.com>
2020-03-18 15:05:26 +0530

    X00T.sh : Update for Andorid 10

--- X00T.sh
+++ X00T.sh
@@ -1,7 +1,7 @@
-git clone https://github.com/ironmerc/device_asus_X00T -b aosip device/asus/X00T
-git clone https://github.com/KudProject/kernel_asus_sdm660 -b lineage-16.0 kernel/asus/sdm660
-git clone https://github.com/ironmerc/vendor_asus vendor/asus
-rm -rf h*/q*/media-caf h*/q*/display-caf h*/q*/audio-caf
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-16.0-caf-8998 hardware/qcom/media-caf/msm8998
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-16.0-caf-8998 hardware/qcom/audio-caf/msm8998
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-16.0-caf-8998 hardware/qcom/display-caf/msm8998
+git clone https://github.com/varunhardgamer/derp_x00t device/asus/X00T
+git clone https://github.com/varunhardgamer/vendor_x00t vendor/asus
+git clone https://github.com/pranavkapoor001/kernel_asus_sdm660 kernel/asus/sdm660
+rm -rf h*/q*-caf/msm8998/media h*/q*-caf/msm8998/display h*/q*-caf/msm8998/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/display
