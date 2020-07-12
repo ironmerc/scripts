bfe21abee3a2d53f82fa6a023376890015a746fd
Akshat Sinha <sinhaakshat2012@gmail.com>
2020-07-12 12:55:18 +0530

    gdrive.sh: Use new api

--- gdrive.sh
+++ gdrive.sh
@@ -1,6 +1,2 @@
 cd ~
-wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
-mv uc?id* gdrive
-chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive
-gdrive list
+wget https://raw.githubusercontent.com/usmanmughalji/gdriveupload/master/gdrive && chmod +x gdrive && sudo install gdrive /usr/local/bin/gdrive && gdrive list
