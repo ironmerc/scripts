1a854a09ac71e68d007c64207567b2b96416a78c
Akshat Sinha <sinhaakshat2012@gmail.com>
2020-10-02 23:34:17 +0530

    Partial Update For Android 11

--- X00T.sh
+++ X00T.sh
@@ -2,6 +2,6 @@ git clone https://github.com/varunhardgamer/derp_x00t device/asus/X00T
 git clone https://github.com/varunhardgamer/vendor_x00t vendor/asus
 git clone https://github.com/pranavkapoor001/kernel_asus_sdm660 kernel/asus/sdm660
 rm -rf h*/q*-caf/msm8998/media h*/q*-caf/msm8998/display h*/q*-caf/msm8998/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/media
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/display
