51b9ec0633bc7e4a1e3096a85bf64aa5c87c0416
Akshat Sinha <sinhaakshat2012@gmail.com>
2020-11-23 18:41:12 +0530

    Update for X00T
    RIP land

--- hals.sh
+++ hals.sh
@@ -1,4 +1,4 @@
-rm -rf h*/q*/media-caf h*/q*/audio-caf h*/q*/display-caf 
-git clone https://github.com/RiteshSaxena/hardware_qcom_audio -b pie-caf-8996 hardware/qcom/audio-caf/msm8996
-git clone https://github.com/RiteshSaxena/hardware_qcom_display -b pie-caf-8996 hardware/qcom/display-caf/msm8996
-git clone https://github.com/RiteshSaxena/hardware_qcom_media -b pie-caf-8996 hardware/qcom/media-caf/msm8996
+rm -rf h*/q*-caf/msm8998/media h*/q*-caf/msm8998/display h*/q*-caf/msm8998/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/display
