b502b58ae35702e4c40a9c5f54c4fdc5a60fa272
Akshat Sinha <sinhaakshat2012@gmail.com>
2020-11-27 17:08:24 +0530

    Re order the commands

--- derp.sh
+++ derp.sh
@@ -6,10 +6,9 @@ git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18
 git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.0-caf-msm8998 hardware/qcom/display
 rm -rf hardware/qcom/camera 
 rm -rf vendor/qcom/opensource/usb
-cd h*/q*/display
-git revert acdd208e757043c394cabc420c82f89808bd7c39
-cd ../../..
 cd v*/q*/o*/c*/display
 git fetch https://github.com/DerpGang/commonsys_display
 git cherry-pick 8b7a825f55bf629b76db730bf1a0508648f63b80
 cd ../../../../..
+cd h*/q*/display
+git revert acdd208e757043c394cabc420c82f89808bd7c39
