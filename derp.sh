0cb58da3c363b15d363a33b041f235b535c48fc4
Akshat Sinha <sinhaakshat2012@gmail.com>
2020-11-27 16:10:56 +0530

    Script for derp caf

---
+++ derp.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/ironmerc/device_asus_X00T-priv -b derp device/asus/X00T
+git clone https://github.com/ironmerc/vendor_asus-priv vendor/asus 
+git clone https://gitlab.com/Pranavkapoor001/kernel_asus_sdm660 kernel/asus/sdm660
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8998 hardware/qcommedia
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8998 hardware/qcom/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.0-caf-msm8998 hardware/qcom/display
+rm -rf hardware/qcom/camera 
+rm -rf vendor/qcom/opensource/usb
+cd h*/q*/display
+git revert acdd208e757043c394cabc420c82f89808bd7c39
+cd ../../..
